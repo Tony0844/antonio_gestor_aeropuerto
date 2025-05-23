28a4ab8d59c8e4782c2afeacf6bd8e68ec8f051b
Tony0844 <0tabletantonio1@gmail.com>
2025-05-23 17:27:00 +0200

    Update util.py

--- antonio_gestor_aeropuerto/util.py
+++ antonio_gestor_aeropuerto/util.py
@@ -5,22 +5,20 @@ from time import sleep
 
 init(autoreset=True)
 
-aeropuertos = [
-    {"codigo": "MAD", "nombre": "Aeropuerto Adolfo Suárez Madrid-Barajas", "ciudad": "Madrid"},
-    {"codigo": "BCN", "nombre": "Aeropuerto Josep Tarradellas Barcelona-El Prat", "ciudad": "Barcelona"},
-    {"codigo": "AGP", "nombre": "Aeropuerto de Málaga-Costa del Sol", "ciudad": "Málaga"},
-    {"codigo": "SVQ", "nombre": "Aeropuerto de Sevilla", "ciudad": "Sevilla"}
-]
+aeropuertos =[] 
 
 vuelos = [
-    {"origen": "MAD", "destino": "BCN"},
-    {"origen": "AGP", "destino": "SVQ"},
-    {"origen": "BCN", "destino": "MAD"},
-    {"origen": "SVQ", "destino": "AGP"}
+    {"origen":"madrid","destino": "barcelona","id": "IB101", "km": 504, "plazas": 180}, #Madrid => Barcelona -- 1
+    {"origen": "barcelona","destino": "malaga", "id": "VY450", "km": 770, "plazas": 160}, #Barcelona => Malaga -- 2
+    {"origen": "malaga","destino": "madrid", "id": "UX333", "km": 430, "plazas": 220} #Malaga => Madrid -- 3
+
 ]
 
+km_1 = vuelos[0]['km']
+plazas_1 = vuelos[0]['plazas']
 
-                                                                # Retos opcionales
+
+                                                               # Retos opcionales
 # Editar o eliminar vuelos -- Terminado
 # Guardar / cargar aeropurtos y vuelos 'JSON' -- Terminado
 # Mostrar estadisticas (vuelo mas largo, promedio de km)
@@ -49,8 +47,14 @@ def introduce_codigo(msg):
         if len(codigo) == 3 and codigo.isalpha() and codigo.isupper() and not existe:
             print(Fore.GREEN + f"El codigo IATA '{codigo}' ha sido añadido")
             return codigo
+        elif any(a['codigo'] == codigo for a in aeropuertos):
+            print(Fore.RED + "Error: El codigo ya esta en la lista de aerpuertos")
+        elif any(a['codigo'] == codigo for a in vuelos):
+            print(Fore.RED + "Error: El codigo ya esta en la lista de vuelos")
+        elif not codigo.isupper():
+            print(Fore.RED + "Error: El codigo debe de estar en mayusculas")
         else:
-            print(Fore.RED + "Error: El código debe tener 3 letras en MAYÚSCULAS, no contener símbolos y no estar ya en la lista")
+            print(Fore.RED + "Error: El codigo debe contener solo letras")
  
 # Terminada
 def nuevo_aeropuerto(lista): #lista = aeropuertos
@@ -95,95 +99,14 @@ def nuevo_vuelo(vuelos, aeropuertos):
             print(Fore.RED + "Error: Este vuelo ya existe")
             return
 
-    vuelos.append({"origen": origen, "destino": destino, "km": None})
-    print(Fore.GREEN + f"Vuelo añadido: {origen} → {destino} -- KM:{None}")
+    vuelos.append({"origen": origen, "destino": destino,})
+    print(Fore.GREEN + f"Vuelo añadido: {origen} → {destino}")
+
 
 # Terminado    
 def listar_vuelos(vuelos):
     
-    for i,vuelo in enumerate(vuelos, start=1):
-        print(Fore.YELLOW  + f"{i}.{vuelo['origen']} => {vuelo['destino']} -- KM:{None}")
+    for i, vuelo in enumerate(vuelos, start=1):
+        print(Fore.YELLOW + f"{i}. {vuelo['origen']} => {vuelo['destino']} -- KM: {vuelo['km']}")
 
 # Terminado
-def buscar_por_aeropuerto(vuelos):
-    codigo_a_buscar = input("Código IATA: ").strip()
-    vuelos_filtrados = []
-
-    for vuelo in vuelos:
-        if vuelo['origen'] == codigo_a_buscar or vuelo['destino'] == codigo_a_buscar:
-            vuelos_filtrados.append(vuelo)
-        
-    if vuelos_filtrados:
-        print(Fore.MAGENTA + f"Estos son los vuelos con el código: {codigo_a_buscar}")
-        for i in vuelos_filtrados:
-            print(Fore.YELLOW + f"{i['origen']} => {i['destino']} -- Km: {None}")
-    else:
-        print(f"No se encontraron vuelos con el código {codigo_a_buscar}")
-
-
-# Funciones opcionales
-
-# Terminado
-def editar_eliminar_vuelos(vuelos):
-
-    for n,i in enumerate(vuelos):
-        print(Fore.YELLOW  + f"{n}. {i['origen']} => {i['destino']} -- KM:{None}")
-
-
-    editar = input("¿Que quieres hacer, editar o eliminar?: ").lower()
-    if editar == "eliminar":    # Opcion elegida = opción
-        vuelo_a_eliminar = int(input("¿Que vuelo quieres eliminar?: "))
-        if 0 <= vuelo_a_eliminar < len(vuelos): # Verifica si el numero que ha dado esta dentro de el numero de indices que hay
-            del vuelos[vuelo_a_eliminar]
-            print(Fore.GREEN + f"El vuelo de a se ha eliminado correctamente")
-            for n,i in enumerate(vuelos):
-                print(Fore.YELLOW  + f"{n}. {i['origen']} => {i['destino']} -- KM:{None}")
-        else:
-            print(Fore.RED + "Índice no válido")
-    elif editar == "editar":    # Opcion elegida = editar
-        vuelo_a_editar = int(input("¿Que vuelo quieres editar?: "))
-        if 0 <= vuelo_a_editar < len(vuelos): # Verifica si el numero que ha dado esta dentro de el numero de indices que hay
-            origen_o_destino = input("¿Origen o destino?: ").lower()
-
-            if origen_o_destino == "origen": # Detecta que parte del vuelo quiere editar Origen/Destino
-                while True:
-                    origen_nuevo = input("Codigo IATA del nuevo origen: ")
-                    if origen_nuevo.isupper() and len(origen_nuevo) == 3 and origen_nuevo.isalpha():# Verifica si el código que ha dado el usuario cumple los requisitos del codigo IATA
-                        vuelos[vuelo_a_editar]['origen'] = origen_nuevo
-                        print(Fore.CYAN + "Lista de vuelos actualizada: ")
-                        break
-                    else:
-                        print(Fore.RED + "Error: El codigo debe de estar en mayusculas y contener 3 letras")
-
-                for n, i in enumerate(vuelos):
-                    print(Fore.MAGENTA + f"{n}. {i['origen']} => {i['destino']} -- Km:{None}")
-            
-            elif origen_o_destino == "destino":
-                while True:
-                    destino_nuevo = input("Codigo IATA del nuevo destino: ")
-                    if destino_nuevo.isupper() and len(destino_nuevo) == 3 and destino_nuevo.isalpha(): #Verifica si el código que ha dado el usuario cumple los requisitos del codigo IATA
-                        vuelos[vuelo_a_editar]['destino'] = destino_nuevo
-                        print(Fore.CYAN + "Lista de vuelos actualizadas: ")
-                        break
-                    else:
-                        print(Fore.RED + "Error: El codigo debe de estar en mayusculas y contener 3 letras")
-                
-                for n, i in enumerate(vuelos):
-                    print(Fore.MAGENTA + f"{n}. {i['origen']} => {i['destino']} -- Km:{None}")
-
-
-def guardar_datos(aeropuertos, vuelos, archivo="datos.json"):
-    datos = {
-        "aeropuertos": aeropuertos,
-        "vuelos": vuelos
-    }
-    with open(archivo, "w", encoding="utf-8") as f:
-        json.dump(datos, f, indent=4)
-
-def cargar_datos(archivo="datos.json"):
-    try:
-        with open(archivo, "r", encoding="utf-8") as f:
-            datos = json.load(f)
-            return datos.get("aeropuertos", []), datos.get("vuelos", [])
-    except FileNotFoundError:
-        return [], []
